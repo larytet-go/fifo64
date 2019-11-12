0290880f18704b737325aa216a1662911c686c01
Arkady Miasnikov <larytet@yahoo.com>
2019-11-12 13:20:16 +0200

    Update fifo64.go

--- fifo64.go
+++ fifo64.go
@@ -1,7 +1,5 @@
 package fifo64
 
-type FifoItem uint64
-
 type Fifo struct {
 	head int
 	tail int
@@ -11,7 +9,7 @@ type Fifo struct {
 
 func New(size int) *Fifo {
 	f := new(Fifo)
-	f.data = make([]FifoItem, size+1, size+1)
+	f.data = make([]uint64, size+1, size+1)
 	f.size = size
 	f.head = 0
 	f.tail = 0
@@ -30,7 +28,7 @@ func (f *Fifo) Len() int {
 // problems if there is a race
 // s.head is modified by remove() and is an atomic operation
 // I do not care about valifity of s.tai
-func (f *Fifo) pick() (key uint64, ok bool) {
+func (f *Fifo) Pick() (key uint64, ok bool) {
 	if f.head != f.tail {
 		key = f.data[f.head]
 		return key, true
@@ -39,3 +37,25 @@ func (f *Fifo) pick() (key uint64, ok bool) {
 	}
 }
 
+
+func (f *Fifo) add(key uint64) (ok bool) {
+	newTail := s.inc(s.tail)
+	if s.head != newTail {
+		s.data[s.tail] = key
+		s.tail = newTail
+		return true
+	} else {
+		return false
+	}
+}
+
+func (f *Fifo) remove() (key uint64, ok bool) {
+	newHead := s.inc(s.head)
+	if s.head != s.tail {
+		key = s.data[s.head]
+		s.head = newHead
+		return key, true
+	} else {
+		return key, false
+	}
+}
